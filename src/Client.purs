e6ef11eb513d6aa07a87c5ecd39962fb3540bbe8
Brad Reed <brad@bradreed.co.uk>
2025-01-31 19:46:24 +0000

    improve formatting a bit

--- src/Client.purs
+++ src/Client.purs
@@ -4,14 +4,22 @@ import Prelude
 
 import Data.Argonaut (Json, decodeJson)
 import Data.Argonaut.Parser (jsonParser)
+import Data.Array (head, sort)
 import Data.Bifunctor (lmap)
-import Data.Array (head)
 import Data.DateTime (Date, date)
 import Data.Either (Either, hush, note)
 import Data.Formatter.DateTime (unformatDateTime)
 import Data.Traversable (traverse)
 import Effect.Aff (Aff)
 import Fetch (fetch)
+
+data Bin = All | Recycling
+
+derive instance Eq Bin
+
+printBin :: Bin -> String
+printBin All = "All bins"
+printBin Recycling = "Recycling"
 
 -- C = all, R = recycling
 type Response = Array
@@ -23,13 +31,16 @@ type Response = Array
   , "R3" :: String
   }
 
-type ParsedResponse = 
-  { recycling :: Array Date
-  , all :: Array Date
-  }
+data BinDate = BinDate Bin Date
+
+derive instance Eq BinDate
+instance Ord BinDate where
+  compare (BinDate _ a) (BinDate _ b) = compare a b
+
+type ParsedResponse = Array BinDate
 
 decodeResponse :: Json -> Either String Response
-decodeResponse = decodeJson >>> lmap show 
+decodeResponse = decodeJson >>> lmap show
 
 url :: String
 url = "https://webapps.southglos.gov.uk/Webservices/SGC.RefuseCollectionService/RefuseCollectionService.svc/getCollections/674321"
@@ -42,11 +53,14 @@ getBin = do
   where
 
   parseResponse :: Response -> Either String ParsedResponse
-  parseResponse json = note "No results" do 
+  parseResponse json = note "No results" do
     obj <- head json
-    all <- traverse parseDate [obj."C1", obj."C2", obj."C3"]
-    recycling <- traverse parseDate [obj."R1", obj."R2", obj."R3"]
-    pure { all, recycling }
+    all <- traverse parseDate [ obj."C1", obj."C2", obj."C3" ]
+    recycling <- traverse parseDate [ obj."R1", obj."R2", obj."R3" ]
+
+    let allDates = (all <#> BinDate All) <> (recycling <#> BinDate Recycling)
+
+    pure $ sort allDates
 
   parseDate = unformatDateTime "DD/MM/YYYY" >>> hush <#> map date
 
--- src/Main.purs
+++ src/Main.purs
@@ -2,15 +2,28 @@ module Main where
 
 import Prelude
 
-import Data.Traversable (traverse_)
-import Debug (traceM)
+import Data.Date (day, month)
+import Data.Either (Either(..))
+import Data.Enum (fromEnum)
+import Data.Foldable (traverse_)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Whatbin.Client (getBin)
+import Effect.Class (liftEffect)
+import Effect.Console as Console
+import Whatbin.Client (BinDate(..), getBin, printBin)
+
+printBinDate :: BinDate -> String
+printBinDate (BinDate bin date) = do
+  let
+    d = fromEnum $ day date
+    m = fromEnum $ month date
+  printBin bin <> " - " <> show d <> "/" <> show m
 
 main :: Effect Unit
 main = launchAff_ do
   res <- getBin
-  traverse_ traceM res
-  pure unit
+  case res of
+    Left err -> liftEffect $ Console.error err
+    Right binDates -> liftEffect do
+      traverse_ (printBinDate >>> Console.log) binDates
 
